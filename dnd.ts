310496aedd8dd030b6c1a8511b1653d69a7dfd55
GabbeKG <el_sueco91@hotmail.com>
2023-02-11 18:20:12 +0100

    ts

--- dnd.ts
+++ dnd.ts
@@ -1,9 +1,12 @@
 var alignment: any[]= [];
 var _classes: any[]=[];
+var races:any[]=[];
 console.log("has started")
 const apiClasses='https://www.dnd5eapi.co/api/classes/?results';
 const apiAlignment='https://www.dnd5eapi.co/api/alignments/?results';
-const getOptions = async function() {
+const apiRaces='https://www.dnd5eapi.co/api/races/?results'
+getOptions();
+async function getOptions() {
     //Alignment
   const alignRes = await fetch(apiAlignment);
   const align= await alignRes.json();
@@ -32,6 +35,20 @@ const getOptions = async function() {
     option.value=_classes[i].name;
     option.text=_classes[i].name;
     classOptions?.appendChild(option);
+  }
+  //RACES
+   const raceRes = await fetch(apiRaces);
+  const race= await raceRes.json();
+  console.log(race);
+  race.results.map((element: any)=>{
+    races.push(element)
+  })
+  const raceOptions= document.querySelector("#raceSelect");
+  for(let i=0;i<races.length;i++){
+    var option=new Option(races[i].name);
+    option.value=races[i].name;
+    option.text=races[i].name;
+    raceOptions?.appendChild(option);
   }
 }
 
